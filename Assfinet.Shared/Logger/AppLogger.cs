7d24b02d31cdc88a27d4c46366461a408dc9325b
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-09 13:45:19 +0200

    Log methoden erstellt

---
+++ Assfinet.Shared/Logger/AppLogger.cs
@@ -0,0 +1,31 @@
+using Assfinet.Shared.Contracts;
+using Serilog;
+
+namespace Assfinet.Shared.Logger
+{
+    public class AppLogger : IAppLogger
+    {
+        private readonly ILogger _logger;
+
+        public AppLogger()
+        {
+            _logger = Log.Logger;
+        }
+
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.Information(message, args);
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.Warning(message, args);
+        }
+
+        public void LogError(string message, params object[] args)
+        {
+            _logger.Error(message, args);
+        }
+        
+    }
+}

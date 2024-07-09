fe006480bad8fe6116ee3c4ff08cecb3dac467e4
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-09 13:47:51 +0200

    Initial

---
+++ Assfinet.Shared/Contracts/IAppLogger.cs
@@ -0,0 +1,9 @@
+namespace Assfinet.Shared.Contracts
+{
+    public interface IAppLogger
+    {
+        void LogInformation(string message, params object[] args);
+        void LogWarning(string message, params object[] args);
+        void LogError(string message, params object[] args);
+    }
+}

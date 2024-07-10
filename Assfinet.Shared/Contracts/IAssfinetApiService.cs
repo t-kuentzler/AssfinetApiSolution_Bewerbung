15c19f51989e4553d2f506623053aec84a5c1861
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-10 16:21:48 +0200

    gelöscht

--- Assfinet.Shared/Contracts/IAssfinetApiService.cs
+++
@@ -1,8 +0,0 @@
-using Assfinet.Shared.Models;
-
-namespace Assfinet.Shared.Contracts;
-
-public interface IAssfinetApiService
-{
-    Task<List<KundeModel>> GetKundenAsync();
-}
--- Assfinet.Shared/Services/AssfinetApiService.cs
+++
@@ -1,8 +0,0 @@
-using Assfinet.Shared.Contracts;
-
-namespace Assfinet.Shared.Services;
-
-public class AssfinetApiService : IAssfinetApiService
-{
-
-}

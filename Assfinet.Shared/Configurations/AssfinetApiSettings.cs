f8605ffd59598400de865acb93ec2fad61bb619a
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-10 16:21:57 +0200

    Eigenschaften konfiguriert

---
+++ Assfinet.Shared/Configurations/AssfinetApiSettings.cs
@@ -0,0 +1,13 @@
+namespace Assfinet.Shared.Configurations
+{
+    public class AssfinetApiSettings
+    {
+        public string BaseUriApi { get; set; }
+        public string BaseUriAuth { get; set; }
+        public string UserName { get; set; }
+        public string Passwort { get; set; }
+        public string License { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}

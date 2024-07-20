355060d1420184bd517731f84c85fa4a310fb5cc
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-20 17:10:31 +0200

    Passwort zu Password geändert

--- Assfinet.Shared/Configurations/AssfinetApiSettings.cs
+++ Assfinet.Shared/Configurations/AssfinetApiSettings.cs
@@ -5,7 +5,7 @@ namespace Assfinet.Shared.Configurations
         public string BaseUriApi { get; set; }
         public string BaseUriAuth { get; set; }
         public string UserName { get; set; }
-        public string Passwort { get; set; }
+        public string Password { get; set; }
         public string License { get; set; }
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }

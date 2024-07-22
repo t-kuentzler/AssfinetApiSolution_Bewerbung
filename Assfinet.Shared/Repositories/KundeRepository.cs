dbc16d651a6ba4f551afe62566556842cb8c4a68
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-22 16:08:00 +0200

    AddKundeAsync überarbeitet

--- Assfinet.Shared/Repositories/KundeRepository.cs
+++ Assfinet.Shared/Repositories/KundeRepository.cs
@@ -1,17 +1,28 @@
 using Assfinet.Shared.Contracts;
 using Assfinet.Shared.Entities;
+using Assfinet.Shared.Exceptions;
 
 namespace Assfinet.Shared.Repositories;
 
 public class KundeRepository : IKundeRepository
 {
-    public KundeRepository()
+    private readonly ApplicationDbContext _applicationDbContext;
+
+    public KundeRepository(ApplicationDbContext applicationDbContext)
     {
-        
+        _applicationDbContext = applicationDbContext;
     }
 
     public async Task AddKundeAsync(Kunde kunde)
     {
-        
+        try
+        {
+            _applicationDbContext.Kunden.Add(kunde);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new RepositoryException($"Ein unerwarteter Fehler ist aufgetreten. KundeId: '{kunde.Id}', AmsId: '{kunde.AmsId}'.", ex);
+        }
     }
 }

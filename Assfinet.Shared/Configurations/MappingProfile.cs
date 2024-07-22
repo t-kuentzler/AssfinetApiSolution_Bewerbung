11f96fd58f1224e6cf1e16c0c70861497f9381f0
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-22 16:46:05 +0200

    CreateMap<KundeModel, Kunde>() erstellt

--- Assfinet.Shared/Configurations/MappingProfile.cs
+++ Assfinet.Shared/Configurations/MappingProfile.cs
@@ -8,6 +8,9 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<KundeModel, Kunde>();
+        CreateMap<KundeModel, Kunde>()
+            .ForMember(dest => dest.AmsId,
+                opt => opt.MapFrom(src => src.Id)) // Mapping von Id in KundeModel auf AmsId in Kunde
+            .ForMember(dest => dest.Id, opt => opt.Ignore()); // ID in Kunde wird von der Datenbank generiert
     }
 }

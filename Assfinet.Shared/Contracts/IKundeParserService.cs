f6c5a5d49ebc88a1c62fd31707faf1d424257099
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-22 15:54:49 +0200

    ParseKundeModelToDbEntity erstellt

--- Assfinet.Shared/Contracts/IKundeParserService.cs
+++ Assfinet.Shared/Contracts/IKundeParserService.cs
@@ -1,6 +1,9 @@
+using Assfinet.Shared.Entities;
+using Assfinet.Shared.Models;
+
 namespace Assfinet.Shared.Contracts;
 
 public interface IKundeParserService
 {
-    
+    Kunde ParseKundeModelToDbEntity(KundeModel kundeModel);
 }
--- Assfinet.Shared/Services/KundeParserService.cs
+++ Assfinet.Shared/Services/KundeParserService.cs
@@ -1,8 +1,18 @@
 using Assfinet.Shared.Contracts;
+using Assfinet.Shared.Entities;
+using Assfinet.Shared.Models;
 
 namespace Assfinet.Shared.Services;
 
 public class KundeParserService : IKundeParserService
 {
-    
+    public KundeParserService()
+    {
+        
+    }
+
+    public Kunde ParseKundeModelToDbEntity(KundeModel kundeModel)
+    {
+        return new Kunde();
+    }
 }

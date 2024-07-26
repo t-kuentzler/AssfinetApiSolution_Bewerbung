4c16298c411142037016e98451f3e9e28f067acc
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-26 13:30:59 +0200

    Initial

---
+++ Assfinet.Shared/Migrations/20240726112825_ChangedKundeZuordnungLengthTo20.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assfinet.Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedKundeZuordnungLengthTo20 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Zuordnung",
+                table: "Kunden",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(3)",
+                oldMaxLength: 3,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Zuordnung",
+                table: "Kunden",
+                type: "varchar(3)",
+                maxLength: 3,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldMaxLength: 20,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}

4c12fde7a6d33d79d568c79168b98a5901d7b4c5
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-30 16:19:37 +0200

    ProcessImportKundeAsync refactoring

--- Assfinet.Shared/Services/KundeProcessingService.cs
+++ Assfinet.Shared/Services/KundeProcessingService.cs
@@ -9,7 +9,8 @@ public class KundeProcessingService : IKundeProcessingService
     private readonly IKundeRepository _kundeRepository;
     private readonly IAppLogger _logger;
 
-    public KundeProcessingService(IValidatorWrapper<Kunde> kundeValidator, IKundeRepository kundeRepository, IAppLogger logger)
+    public KundeProcessingService(IValidatorWrapper<Kunde> kundeValidator, IKundeRepository kundeRepository,
+        IAppLogger logger)
     {
         _kundeValidator = kundeValidator;
         _kundeRepository = kundeRepository;
@@ -23,20 +24,18 @@ public class KundeProcessingService : IKundeProcessingService
 
     public async Task ProcessImportKundeAsync(Kunde kunde)
     {
-        var kundeExists = await _kundeRepository.KundeExistsByAmsIdAsync(kunde.AmsId);
+        var kundeExists = await _kundeRepository.GetKundeByAmsIdAsync(kunde.AmsId);
 
-        if (kundeExists == null)
-        {
-            _logger.LogInformation(
-                $"Es wird versucht, den Kunden mit der AmsId '{kunde.AmsId}' in der Datenbank zu erstellen.");
-            await _kundeRepository.AddKundeAsync(kunde);
-            _logger.LogInformation(
-                $"Der Kunde mit der AmsId '{kunde.AmsId}' wurde erfolgreich in der Datenbank erstellt.");
-        }
-        else
+        if (kundeExists != null)
         {
             _logger.LogError(
                 $"Der Kunde mit der AmsId '{kunde.AmsId}' konnte nicht in der Datenbank erstellt werden, da schon ein Datensatz mit der AmsId existiert.");
         }
+
+        _logger.LogInformation(
+            $"Es wird versucht, den Kunden mit der AmsId '{kunde.AmsId}' in der Datenbank zu erstellen.");
+        await _kundeRepository.AddKundeAsync(kunde);
+        _logger.LogInformation(
+            $"Der Kunde mit der AmsId '{kunde.AmsId}' wurde erfolgreich in der Datenbank erstellt.");
     }
 }

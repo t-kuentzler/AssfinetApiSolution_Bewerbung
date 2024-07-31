9daebe277e71a6c59b6129c280674b8eb29c6b19
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-31 14:18:15 +0200

    ValidateAsync und ProcessImportSparteAsync erstellt

--- Assfinet.Shared/Contracts/ISparteProcessingService.cs
+++ Assfinet.Shared/Contracts/ISparteProcessingService.cs
@@ -2,5 +2,6 @@ namespace Assfinet.Shared.Contracts;
 
 public interface ISparteProcessingService
 {
-    
+    Task ValidateAsync<T>(T sparte) where T : class, IVertragKeyProvider;
+    Task ProcessImportSparteAsync<T>(T sparte) where T : class, IVertragKeyProvider;
 }

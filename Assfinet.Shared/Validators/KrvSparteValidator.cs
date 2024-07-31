8fb4861da4144b1262fe43a5e13d61610cc7a11e
t-kuentzler <tobias.kuentzler@outlook.de>
2024-07-31 14:18:54 +0200

    regeln erstellt

---
+++ Assfinet.Shared/Validators/KrvSparteValidator.cs
@@ -0,0 +1,23 @@
+using Assfinet.Shared.Entities;
+using FluentValidation;
+
+namespace Assfinet.Shared.Validators;
+
+public class KrvSparteValidator : AbstractValidator<KrvSparte>
+{
+    public KrvSparteValidator()
+    {
+        RuleFor(v => v.AmsId).NotEmpty();
+        RuleFor(v => v.Amsidnr)
+            .NotEmpty()
+            .MaximumLength(40);
+        RuleFor(v => v.Key)
+            .NotEmpty()
+            .MaximumLength(40);
+        RuleFor(v => v.Typ)
+            .NotEmpty()
+            .MaximumLength(50);
+        RuleFor(v => v.License).MaximumLength(40);
+        RuleFor(v => v.LastSynchronisation).NotNull();
+    }
+}

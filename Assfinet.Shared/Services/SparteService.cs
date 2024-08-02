494a223920c41d88d869d597b949e190fccd1f1f
t-kuentzler <tobias.kuentzler@outlook.de>
2024-08-02 19:57:49 +0200

    ImportSpartenDatenAsync dynamisch angepasst

--- Assfinet.Shared/Services/SparteService.cs
+++ Assfinet.Shared/Services/SparteService.cs
@@ -23,52 +23,39 @@ public class SparteService : ISparteService
     }
 
     public async Task ImportSpartenDatenAsync(List<object> spartenModels)
+    {
+        if (spartenModels.Count == 0)
         {
-            if (spartenModels.Count == 0)
+            _logger.LogWarning("Es wurden 0 Sparten-Daten von der API abgerufen.");
+            return;
+        }
+
+        _logger.LogInformation($"Es wurden {spartenModels.Count} Sparten-Daten von der API abgerufen.");
+
+        foreach (var sparteModel in spartenModels)
+        {
+            try
             {
-                _logger.LogWarning("Es wurden 0 Sparten-Daten von der API abgerufen.");
-                return;
+                var parsedSparte = _sparteParserService.ParseSparteModel(sparteModel);
+                await _sparteProcessingService.ValidateAndProcessSparteAsync(parsedSparte);
             }
-
-            _logger.LogInformation($"Es wurden {spartenModels.Count} Sparten-Daten von der API abgerufen.");
-
-            foreach (var sparteModel in spartenModels)
+            catch (ValidationException ex)
             {
-                try
-                {
-                    if (sparteModel is KrvModel krvModel)
-                    {
-                        var krvSparte = _sparteParserService.ParseSparteModelToKrvSparte(krvModel);
-                        await _sparteProcessingService.ValidateKrvSparteAsync(krvSparte);
-                        await _sparteProcessingService.ProcessImportKrvSparteAsync(krvSparte);
-                    }
-                    // Weitere Fälle für andere Spartentypen hinzufügen
-                    else
-                    {
-                        _logger.LogWarning($"Unknown type {sparteModel.GetType().FullName}");
-                        throw new UnknownSparteException("Unbekannte Sparte");
-                    }
-                }
-                catch (ValidationException ex)
-                {
-                    _logger.LogError(
-                        $"Validierungsfehler bei Sparte mit dem Schlüssel '{(sparteModel as dynamic).Key}': {ex.Message}", ex);
-                    throw;
-                }
-                catch (RepositoryException ex)
-                {
-                    _logger.LogError(
-                        $"Repository-Fehler beim Import von Sparte mit dem Schlüssel '{(sparteModel as dynamic).Key}': {ex.Message}",
-                        ex);
-                    throw;
-                }
-                catch (Exception ex)
-                {
-                    _logger.LogError(
-                        $"Unerwarteter Fehler beim Import von Sparte mit dem Schlüssel '{(sparteModel as dynamic).Key}': {ex.Message}",
-                        ex);
-                    throw new SparteServiceException();
-                }
+                _logger.LogError($"Validierungsfehler bei Sparte mit dem Schlüssel '{(sparteModel as dynamic).Key}': {ex.Message}", ex);
+                throw;
+            }
+            catch (RepositoryException ex)
+            {
+                _logger.LogError($"Repository-Fehler beim Import von Sparte mit dem Schlüssel '{(sparteModel as dynamic).Key}': {ex.Message}", ex);
+                throw;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Unerwarteter Fehler beim Import von Sparte mit dem Schlüssel '{(sparteModel as dynamic).Key}': {ex.Message}", ex);
+                throw new SparteServiceException();
             }
         }
+    }
+
+
 }

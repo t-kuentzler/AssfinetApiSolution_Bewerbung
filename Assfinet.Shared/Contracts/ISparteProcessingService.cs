9670f4c9330214ace158d2b1495056243191af21
t-kuentzler <tobias.kuentzler@outlook.de>
2024-08-02 14:55:29 +0200

    gelöscht

--- Assfinet.Shared/Contracts/ISparteProcessingService.cs
+++
@@ -1,7 +0,0 @@
-namespace Assfinet.Shared.Contracts;
-
-public interface ISparteProcessingService
-{
-    Task ValidateAsync<T>(T sparte) where T : class, IVertragKeyProvider;
-    Task ProcessImportSparteAsync<T>(T sparte) where T : class, IVertragKeyProvider;
-}
--- Assfinet.Shared/Contracts/IVertragKeyProvider.cs
+++
@@ -1,7 +0,0 @@
-namespace Assfinet.Shared.Contracts;
-
-public interface IVertragKeyProvider
-{
-    string Key { get; }
-
-}

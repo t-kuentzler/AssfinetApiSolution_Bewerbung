741fb928c117f28f6facca52dd7faf9dd5c6592f
t-kuentzler <tobias.kuentzler@outlook.de>
2024-08-02 14:55:06 +0200

    GetSpartenDatenAsync übergabeparameter angepasst

--- Assfinet.Shared/Contracts/IApiService.cs
+++ Assfinet.Shared/Contracts/IApiService.cs
@@ -1,3 +1,4 @@
+using Assfinet.Shared.Enums;
 using Assfinet.Shared.Models;
 
 namespace Assfinet.Shared.Contracts;
@@ -6,6 +7,6 @@ public interface IApiService
 {
     Task<List<KundeModel>> GetKundenAsync(int skip, int take);
     Task<List<VertragModel>> GetVertraegeAsync(int skip, int take);
-    Task<List<object>> GetSpartenDatenAsync(string sparte, int skip, int take);
+    Task<List<object>> GetSpartenDatenAsync(Spartentypen sparte, int skip, int take);
     
 }

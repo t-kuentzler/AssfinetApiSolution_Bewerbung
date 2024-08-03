331a4d023aaaddca96b04bd80f5dd3bb57851871
t-kuentzler <tobias.kuentzler@outlook.de>
2024-08-03 15:06:59 +0200

    Tests erstellt

--- Assfinet.Shared.Tests/Services/KundeServiceTests.cs
+++ Assfinet.Shared.Tests/Services/KundeServiceTests.cs
@@ -14,7 +14,6 @@ namespace Assfinet.Shared.Tests.Services
         private readonly Mock<IKundeParserService> _kundeParserServiceMock;
         private readonly Mock<IAppLogger> _loggerMock;
         private readonly Mock<IKundeProcessingService> _kundeProcessingServiceMock;
-
         private readonly KundeService _kundeService;
 
         public KundeServiceTests()
@@ -22,46 +21,29 @@ namespace Assfinet.Shared.Tests.Services
             _kundeParserServiceMock = new Mock<IKundeParserService>();
             _loggerMock = new Mock<IAppLogger>();
             _kundeProcessingServiceMock = new Mock<IKundeProcessingService>();
-
-            _kundeService = new KundeService(
-                _kundeParserServiceMock.Object,
-                _loggerMock.Object,
-                _kundeProcessingServiceMock.Object
-            );
+            _kundeService = new KundeService(_kundeParserServiceMock.Object, _loggerMock.Object, _kundeProcessingServiceMock.Object);
         }
 
-        //ImportKundenAsync
         [Fact]
         public async Task ImportKundenAsync_NoCustomers_LogsWarning()
         {
-            // Arrange
             var kundenModels = new List<KundeModel>();
-
-            // Act
             await _kundeService.ImportKundenAsync(kundenModels);
-
-            // Assert
             _loggerMock.Verify(l => l.LogWarning("Es wurden 0 Kunden von der API abgerufen."), Times.Once);
         }
 
         [Fact]
         public async Task ImportKundenAsync_ValidCustomers_ProcessesSuccessfully()
         {
-            // Arrange
-            var kundenModels = new List<KundeModel>
-            {
-                new KundeModel { Id = Guid.NewGuid() }
-            };
+            var kundenModels = new List<KundeModel> { new KundeModel { Id = Guid.NewGuid() } };
             var kunde = new Kunde { Id = 1, AmsId = Guid.NewGuid() };
 
             _kundeParserServiceMock.Setup(p => p.ParseKundeModelToDbEntity(It.IsAny<KundeModel>())).Returns(kunde);
             _kundeProcessingServiceMock.Setup(p => p.ValidateKundeAsync(kunde)).Returns(Task.CompletedTask);
             _kundeProcessingServiceMock.Setup(p => p.ProcessImportKundeAsync(kunde)).Returns(Task.CompletedTask);
 
-            // Act
             await _kundeService.ImportKundenAsync(kundenModels);
 
-            // Assert
             _loggerMock.Verify(l => l.LogInformation($"Es wurden {kundenModels.Count} Kunden von der API abgerufen."), Times.Once);
             _kundeParserServiceMock.Verify(p => p.ParseKundeModelToDbEntity(It.IsAny<KundeModel>()), Times.Once);
             _kundeProcessingServiceMock.Verify(p => p.ValidateKundeAsync(kunde), Times.Once);
@@ -71,69 +53,68 @@ namespace Assfinet.Shared.Tests.Services
         [Fact]
         public async Task ImportKundenAsync_ValidationFails_LogsError()
         {
-            // Arrange
-            var kundenModels = new List<KundeModel>
-            {
-                new KundeModel { Id = Guid.NewGuid() }
-            };
+            var kundenModels = new List<KundeModel> { new KundeModel { Id = Guid.NewGuid() } };
             var kunde = new Kunde { Id = 1, AmsId = Guid.NewGuid() };
             var validationResult = new ValidationResult(new[] { new ValidationFailure("AmsId", "Validation failed") });
 
             _kundeParserServiceMock.Setup(p => p.ParseKundeModelToDbEntity(It.IsAny<KundeModel>())).Returns(kunde);
             _kundeProcessingServiceMock.Setup(p => p.ValidateKundeAsync(kunde)).ThrowsAsync(new ValidationException(validationResult.Errors));
 
-            // Act
             await Assert.ThrowsAsync<ValidationException>(() => _kundeService.ImportKundenAsync(kundenModels));
 
-            // Assert
-            _loggerMock.Verify(
-                l => l.LogError(It.Is<string>(s => s.Contains($"Bei dem Kunden mit der AmsId '{kundenModels[0].Id}' ist ein Validierungsfehler aufgetreten: Validation failed")),
-                    It.IsAny<ValidationException>()),
-                Times.Once);
+            _loggerMock.Verify(l => l.LogError(It.Is<string>(s => s.Contains($"Bei dem Kunden mit der AmsId '{kundenModels[0].Id}' ist ein Validierungsfehler aufgetreten: Validation failed")), It.IsAny<ValidationException>()), Times.Once);
         }
-
-
 
         [Fact]
         public async Task ImportKundenAsync_RepositoryException_LogsError()
         {
-            // Arrange
-            var kundenModels = new List<KundeModel>
-            {
-                new KundeModel { Id = Guid.NewGuid() }
-            };
+            var kundenModels = new List<KundeModel> { new KundeModel { Id = Guid.NewGuid() } };
             var kunde = new Kunde { Id = 1, AmsId = Guid.NewGuid() };
 
             _kundeParserServiceMock.Setup(p => p.ParseKundeModelToDbEntity(It.IsAny<KundeModel>())).Returns(kunde);
             _kundeProcessingServiceMock.Setup(p => p.ValidateKundeAsync(kunde)).Returns(Task.CompletedTask);
             _kundeProcessingServiceMock.Setup(p => p.ProcessImportKundeAsync(kunde)).ThrowsAsync(new RepositoryException("Repository error"));
 
-            // Act
             await Assert.ThrowsAsync<RepositoryException>(() => _kundeService.ImportKundenAsync(kundenModels));
 
-            // Assert
             _loggerMock.Verify(l => l.LogError($"Repository-Exception beim Importieren von dem Kunden mit der AmsId '{kundenModels[0].Id}'.", It.IsAny<RepositoryException>()), Times.Once);
         }
 
         [Fact]
         public async Task ImportKundenAsync_UnexpectedException_LogsError()
         {
-            // Arrange
-            var kundenModels = new List<KundeModel>
-            {
-                new KundeModel { Id = Guid.NewGuid() }
-            };
+            var kundenModels = new List<KundeModel> { new KundeModel { Id = Guid.NewGuid() } };
             var kunde = new Kunde { Id = 1, AmsId = Guid.NewGuid() };
 
             _kundeParserServiceMock.Setup(p => p.ParseKundeModelToDbEntity(It.IsAny<KundeModel>())).Returns(kunde);
             _kundeProcessingServiceMock.Setup(p => p.ValidateKundeAsync(kunde)).Returns(Task.CompletedTask);
             _kundeProcessingServiceMock.Setup(p => p.ProcessImportKundeAsync(kunde)).ThrowsAsync(new Exception("Unexpected error"));
 
-            // Act
             await Assert.ThrowsAsync<KundeServiceException>(() => _kundeService.ImportKundenAsync(kundenModels));
 
-            // Assert
             _loggerMock.Verify(l => l.LogError($"Es ist ein unerwarteter Fehler beim Importieren von dem Kunden mit der AmsId '{kundenModels[0].Id}' aufgetreten.", It.IsAny<Exception>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task ImportKundenAsync_ArgumentNullException_LogsError()
+        {
+            var kundenModels = new List<KundeModel> { new KundeModel { Id = Guid.NewGuid() } };
+            _kundeParserServiceMock.Setup(p => p.ParseKundeModelToDbEntity(It.IsAny<KundeModel>())).Throws(new ArgumentNullException());
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => _kundeService.ImportKundenAsync(kundenModels));
+
+            _loggerMock.Verify(l => l.LogError(It.IsAny<string>(), It.IsAny<ArgumentNullException>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task ImportKundenAsync_InvalidOperationException_LogsError()
+        {
+            var kundenModels = new List<KundeModel> { new KundeModel { Id = Guid.NewGuid() } };
+            _kundeParserServiceMock.Setup(p => p.ParseKundeModelToDbEntity(It.IsAny<KundeModel>())).Throws(new InvalidOperationException());
+
+            await Assert.ThrowsAsync<InvalidOperationException>(() => _kundeService.ImportKundenAsync(kundenModels));
+
+            _loggerMock.Verify(l => l.LogError(It.IsAny<string>(), It.IsAny<InvalidOperationException>()), Times.Once);
         }
     }
 }

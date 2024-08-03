6731b962ecafcc6d59403d54e3e176144c0a7fdf
t-kuentzler <tobias.kuentzler@outlook.de>
2024-08-03 14:30:54 +0200

    Eigenschaften erstellt

--- Assfinet.Shared/Models/DepModel.cs
+++ Assfinet.Shared/Models/DepModel.cs
@@ -1,6 +1,43 @@
 namespace Assfinet.Shared.Models;
 
-public class DepModel
+public class DepModel : VertragSparteModel
 {
-    
+    public string DEP101 { get; set; }
+    public string DEP111 { get; set; }
+    public string DEP113 { get; set; }
+    public string DEP115 { get; set; }
+    public string DEP116 { get; set; }
+    public string DEP119 { get; set; }
+    public string DEP120 { get; set; }
+    public string DEP122 { get; set; }
+    public string DEP125 { get; set; }
+    public string DEP126 { get; set; }
+    public string DEP128 { get; set; }
+    public string DEP131 { get; set; }
+    public string DEP132 { get; set; }
+    public string DEP134 { get; set; }
+    public string DEP137 { get; set; }
+    public string DEP138 { get; set; }
+    public string DEP140 { get; set; }
+    public string DEP143 { get; set; }
+    public string DEP144 { get; set; }
+    public string DEP146 { get; set; }
+    public string DEP149 { get; set; }
+    public string DEP150 { get; set; }
+    public string DEP152 { get; set; }
+    public string DEP155 { get; set; }
+    public string DEP156 { get; set; }
+    public string DEP158 { get; set; }
+    public string DEP161 { get; set; }
+    public string DEP162 { get; set; }
+    public string DEP164 { get; set; }
+    public string DEP167 { get; set; }
+    public string DEP168 { get; set; }
+    public string DEP170 { get; set; }
+    public string DEP173 { get; set; }
+    public string DEP174 { get; set; }
+    public string DEP176 { get; set; }
+    public string DEP178 { get; set; }
+    public string DEP179 { get; set; }
+    public string DEP180 { get; set; }
 }

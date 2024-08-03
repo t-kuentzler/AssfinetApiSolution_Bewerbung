39fdd7b74f929342b2897483d75da41c8ff496da
t-kuentzler <tobias.kuentzler@outlook.de>
2024-08-03 15:10:43 +0200

    Fehlerbehandlung entfernt

--- Assfinet.Shared/Services/VertragParserService.cs
+++ Assfinet.Shared/Services/VertragParserService.cs
@@ -1,6 +1,5 @@
 using Assfinet.Shared.Contracts;
 using Assfinet.Shared.Entities;
-using Assfinet.Shared.Exceptions;
 using Assfinet.Shared.Models;
 using AutoMapper;
 
@@ -9,42 +8,30 @@ namespace Assfinet.Shared.Services;
 public class VertragParserService : IVertragParserService
 {
     private readonly IMapper _mapper;
-    private readonly IAppLogger _logger;
 
-    public VertragParserService(IMapper mapper, IAppLogger logger)
+    public VertragParserService(IMapper mapper)
     {
         _mapper = mapper;
-        _logger = logger;
     }
-    
+
     public Vertrag ParseKundeModelToDbEntity(VertragModel vertragModel)
     {
-        try
+        if (vertragModel == null)
         {
-            if (vertragModel == null)
-            {
-                throw new ArgumentNullException(nameof(vertragModel));
-            }
-
-            var vertrag = _mapper.Map<Vertrag>(vertragModel);
-            if (vertrag == null)
-            {
-                throw new InvalidOperationException("Mapping von VertragModel zu Vertrag fehlgeschlagen.");
-            }
-
-            vertrag.Finanzen = _mapper.Map<VertragFinanzen>(vertragModel) ?? new VertragFinanzen();
-            vertrag.VertragDetails = _mapper.Map<VertragDetails>(vertragModel) ?? new VertragDetails();
-            vertrag.Historie = _mapper.Map<VertragHistorie>(vertragModel) ?? new VertragHistorie();
-            vertrag.BankDetails = _mapper.Map<VertragBank>(vertragModel) ?? new VertragBank();
-
-            return vertrag;
+            throw new ArgumentNullException(nameof(vertragModel));
         }
-        catch (Exception ex)
+
+        var vertrag = _mapper.Map<Vertrag>(vertragModel);
+        if (vertrag == null)
         {
-            _logger.LogError(
-                $"Es ist ein unerwarteter Fehler beim Parsen von KundeModel zu Kunde aufgetreten.",
-                ex);
-            throw new VertragParserServiceException();
+            throw new InvalidOperationException("Mapping von VertragModel zu Vertrag fehlgeschlagen.");
         }
+
+        vertrag.Finanzen = _mapper.Map<VertragFinanzen>(vertragModel) ?? new VertragFinanzen();
+        vertrag.VertragDetails = _mapper.Map<VertragDetails>(vertragModel) ?? new VertragDetails();
+        vertrag.Historie = _mapper.Map<VertragHistorie>(vertragModel) ?? new VertragHistorie();
+        vertrag.BankDetails = _mapper.Map<VertragBank>(vertragModel) ?? new VertragBank();
+
+        return vertrag;
     }
 }

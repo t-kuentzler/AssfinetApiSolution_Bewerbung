63a0d055f034f52f5d99fe1b0a81bb5ff5b30f76
t-kuentzler <tobias.kuentzler@outlook.de>
2024-08-03 14:48:26 +0200

    formatiert

--- Assfinet.InitialImporter.Api/Controllers/DataController.cs
+++ Assfinet.InitialImporter.Api/Controllers/DataController.cs
@@ -3,142 +3,141 @@ using Assfinet.Shared.Enums;
 using Assfinet.Shared.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Assfinet.InitialImporter.Api.Controllers
+namespace Assfinet.InitialImporter.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DataController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DataController : ControllerBase
+    private readonly IApiService _apiService;
+    private readonly IKundeService _kundeService;
+    private readonly IVertragService _vertragService;
+    private readonly IAppLogger _logger;
+    private readonly ISparteService _sparteService;
+
+    public DataController(IApiService apiService, IKundeService kundeService,
+        IVertragService vertragService, IAppLogger logger, ISparteService sparteService)
     {
-        private readonly IApiService _apiService;
-        private readonly IKundeService _kundeService;
-        private readonly IVertragService _vertragService;
-        private readonly IAppLogger _logger;
-        private readonly ISparteService _sparteService;
+        _apiService = apiService;
+        _kundeService = kundeService;
+        _vertragService = vertragService;
+        _logger = logger;
+        _sparteService = sparteService;
+    }
 
-        public DataController(IApiService apiService, IKundeService kundeService, 
-            IVertragService vertragService, IAppLogger logger, ISparteService sparteService)
+    [HttpPost("import-kunden")]
+    public async Task<IActionResult> ImportKunden()
+    {
+        try
         {
-            _apiService = apiService;
-            _kundeService = kundeService;
-            _vertragService = vertragService;
-            _logger = logger;
-            _sparteService = sparteService;
-        }
+            int skip = 0;
+            int take = 50;
+            bool hasMoreData = true;
+            var allKunden = new List<KundeModel>();
 
-        [HttpPost("import-kunden")]
-        public async Task<IActionResult> ImportKunden()
-        {
-            try
+            while (hasMoreData)
             {
-                int skip = 0;
-                int take = 50;
-                bool hasMoreData = true;
-                var allKunden = new List<KundeModel>();
-
-                while (hasMoreData)
+                var kundenModels = await _apiService.GetKundenAsync(skip, take);
+                if (kundenModels.Count < take)
                 {
-                    var kundenModels = await _apiService.GetKundenAsync(skip, take);
-                    if (kundenModels.Count < take)
-                    {
-                        hasMoreData = false;
-                    }
-
-                    if (kundenModels.Count > 0)
-                    {
-                        await _kundeService.ImportKundenAsync(kundenModels);
-                        allKunden.AddRange(kundenModels);
-                    }
-
-                    skip += take;
-                    await Task.Delay(3000);
+                    hasMoreData = false;
                 }
 
-                _logger.LogInformation($"Es wurden insgesamt {allKunden.Count} Kunden importiert.");
-                return Ok("Import abgeschlossen.");
-            }
-            catch (Exception)
-            {
-                return StatusCode(500, "Interner Serverfehler");
-            }
-        }
-
-        [HttpPost("import-vertraege")]
-        public async Task<IActionResult> ImportVertraege()
-        {
-            try
-            {
-                int skip = 0;
-                int take = 50;
-                bool hasMoreData = true;
-                var allVertraege = new List<VertragModel>();
-
-                while (hasMoreData)
+                if (kundenModels.Count > 0)
                 {
-                    var vertraegeModels = await _apiService.GetVertraegeAsync(skip, take);
-                    if (vertraegeModels.Count < take)
-                    {
-                        hasMoreData = false;
-                    }
-
-                    if (vertraegeModels.Count > 0)
-                    {
-                        await _vertragService.ImportVertraegeAsync(vertraegeModels);
-                        allVertraege.AddRange(vertraegeModels);
-                    }
-
-                    skip += take;
-                    await Task.Delay(3000);
+                    await _kundeService.ImportKundenAsync(kundenModels);
+                    allKunden.AddRange(kundenModels);
                 }
 
-                _logger.LogInformation($"Es wurden insgesamt {allVertraege.Count} Verträge importiert.");
-                return Ok("Import abgeschlossen.");
+                skip += take;
+                await Task.Delay(3000);
             }
-            catch (Exception)
-            {
-                return StatusCode(500, "Interner Serverfehler");
-            }
+
+            _logger.LogInformation($"Es wurden insgesamt {allKunden.Count} Kunden importiert.");
+            return Ok("Import abgeschlossen.");
         }
-
-        [HttpPost("import-sparten")]
-        public async Task<IActionResult> ImportSpartenDaten([FromQuery] Spartentypen? sparte)
+        catch (Exception)
         {
-            if (!sparte.HasValue)
-            {
-                return BadRequest("Kein gültiger Spartentyp ausgewählt.");
-            }
-            
-            try
-            {
-                int skip = 0;
-                int take = 5;
-                bool hasMoreData = true;
+            return StatusCode(500, "Interner Serverfehler");
+        }
+    }
 
-                while (hasMoreData)
+    [HttpPost("import-vertraege")]
+    public async Task<IActionResult> ImportVertraege()
+    {
+        try
+        {
+            int skip = 0;
+            int take = 50;
+            bool hasMoreData = true;
+            var allVertraege = new List<VertragModel>();
+
+            while (hasMoreData)
+            {
+                var vertraegeModels = await _apiService.GetVertraegeAsync(skip, take);
+                if (vertraegeModels.Count < take)
                 {
-                    //Daten abrufen und dynamisch speichern
-                    var spartenDaten = await _apiService.GetSpartenDatenAsync(sparte.Value, skip, take);
-                    if (spartenDaten.Count < take)
-                    {
-                        hasMoreData = false;
-                    }
-
-                    if (spartenDaten.Count > 0)
-                    {
-                        await _sparteService.ImportSpartenDatenAsync(spartenDaten);
-                    }
-
-                    skip += take;
-                    await Task.Delay(3000);
+                    hasMoreData = false;
                 }
 
-                _logger.LogInformation("Import abgeschlossen.");
-                return Ok("Import abgeschlossen.");
+                if (vertraegeModels.Count > 0)
+                {
+                    await _vertragService.ImportVertraegeAsync(vertraegeModels);
+                    allVertraege.AddRange(vertraegeModels);
+                }
+
+                skip += take;
+                await Task.Delay(3000);
             }
-            catch (Exception ex)
+
+            _logger.LogInformation($"Es wurden insgesamt {allVertraege.Count} Verträge importiert.");
+            return Ok("Import abgeschlossen.");
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, "Interner Serverfehler");
+        }
+    }
+
+    [HttpPost("import-sparten")]
+    public async Task<IActionResult> ImportSpartenDaten([FromQuery] Spartentypen? sparte)
+    {
+        if (!sparte.HasValue)
+        {
+            return BadRequest("Kein gültiger Spartentyp ausgewählt.");
+        }
+
+        try
+        {
+            int skip = 0;
+            int take = 50;
+            bool hasMoreData = true;
+
+            while (hasMoreData)
             {
-                _logger.LogError($"Fehler beim Importieren der Sparten-Daten: {ex.Message}");
-                return StatusCode(500, "Interner Serverfehler");
+                //Daten abrufen und dynamisch speichern
+                var spartenDaten = await _apiService.GetSpartenDatenAsync(sparte.Value, skip, take);
+                if (spartenDaten.Count < take)
+                {
+                    hasMoreData = false;
+                }
+
+                if (spartenDaten.Count > 0)
+                {
+                    await _sparteService.ImportSpartenDatenAsync(spartenDaten);
+                }
+
+                skip += take;
+                await Task.Delay(3000);
             }
+
+            _logger.LogInformation("Import abgeschlossen.");
+            return Ok("Import abgeschlossen.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Fehler beim Importieren der Sparten-Daten: {ex.Message}");
+            return StatusCode(500, "Interner Serverfehler");
         }
     }
 }

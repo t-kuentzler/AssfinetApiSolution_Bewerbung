1c83011dae089dfaf3433e53c858ee8e543c8d8c
t-kuentzler <tobias.kuentzler@outlook.de>
2024-08-05 16:16:13 +0200

    Eigenschaften erstellt

---
+++ Assfinet.Shared/Models/ImoModel.cs
@@ -0,0 +1,29 @@
+namespace Assfinet.Shared.Models
+{
+    public class ImoModel : VertragSparteModel
+    {
+        public string IMO101 { get; set; }
+        public string IMO102 { get; set; }
+        public string IMO104 { get; set; }
+        public string IMO105 { get; set; }
+        public string IMO106 { get; set; }
+        public string IMO109 { get; set; }
+        public string IMO110 { get; set; }
+        public string IMO114 { get; set; }
+        public string IMO115 { get; set; }
+        public string IMO116 { get; set; }
+        public string IMO117 { get; set; }
+        public string IMO118 { get; set; }
+        public string IMO119 { get; set; }
+        public string IMO121 { get; set; }
+        public string IMO122 { get; set; }
+        public string IMO123 { get; set; }
+        public string IMO125 { get; set; }
+        public string IMO126 { get; set; }
+        public string IMO127 { get; set; }
+        public string IMO128 { get; set; }
+        public string IMO130 { get; set; }
+        public string IMO131 { get; set; }
+        public string IMO132 { get; set; }
+    }
+}

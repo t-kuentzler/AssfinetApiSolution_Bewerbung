c29598b90cede572686adaa686ff1265d9409b7e
t-kuentzler <tobias.kuentzler@outlook.de>
2024-08-06 21:33:41 +0200

    spezifische sparten mappings entfernt

--- Assfinet.Shared/Configurations/MappingProfile.cs
+++ Assfinet.Shared/Configurations/MappingProfile.cs
@@ -39,49 +39,11 @@ public class MappingProfile : Profile
         CreateMap<VertragModel, VertragBank>()
             .ForMember(dest => dest.VertragId, opt => opt.Ignore());
 
-        CreateMap<KrvModel, KrvSparte>()
-            .ForMember(dest => dest.AmsId,
-                opt => opt.MapFrom(src => src.Id))
-            .ForMember(dest => dest.Id, opt => opt.Ignore()); // ID wird von der Datenbank generiert
-
-        CreateMap<DepModel, DepSparte>()
-            .ForMember(dest => dest.AmsId,
-                opt => opt.MapFrom(src => src.Id)) // Mapping von Id auf AmsId
-            .ForMember(dest => dest.Id, opt => opt.Ignore()); // ID wird von der Datenbank generiert
-
-        CreateMap<ImoModel, ImoSparte>()
-            .ForMember(dest => dest.AmsId,
-                opt => opt.MapFrom(src => src.Id)) // Mapping von Id auf AmsId
-            .ForMember(dest => dest.Id, opt => opt.Ignore()); // ID wird von der Datenbank generiert
-
         CreateMap<VertragSparteModel, Sparte>()
             .ForMember(dest => dest.Id, opt => opt.Ignore()) // ID wird von der Datenbank generiert
             .ForMember(dest => dest.AmsId, opt => opt.MapFrom(src => src.Id));
 
         CreateMap<UnfModel, Sparte>()
             .IncludeBase<VertragSparteModel, Sparte>();
-        
     }
-
-   
-        
-        
-        // Mapping für SparteFields basierend auf den nicht direkt zugeordneten Eigenschaften
-        
-        // CreateMap<object, ICollection<SparteFields>>()
-        //     .ConstructUsing((src, context) => {
-        //         var fields = new List<SparteFields>();
-        //         var sparteProperties = typeof(Sparte).GetProperties().Select(p => p.Name).ToList();
-        //         foreach (var property in src.GetType().GetProperties())
-        //         {
-        //             if (!sparteProperties.Contains(property.Name) && property.CanRead)
-        //             {
-        //                 fields.Add(new SparteFields {
-        //                     FieldName = property.Name,
-        //                     FieldValue = property.GetValue(src)?.ToString()
-        //                 });
-        //             }
-        //         }
-        //         return fields;
-        //     });
-    }
+}

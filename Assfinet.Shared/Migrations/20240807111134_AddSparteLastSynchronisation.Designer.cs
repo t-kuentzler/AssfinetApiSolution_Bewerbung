ab91dccede8f8d2bc18d12b907fa30f3df2044bf
t-kuentzler <tobias.kuentzler@outlook.de>
2024-08-07 13:12:41 +0200

    Initial

--- Assfinet.Shared/Migrations/20240807111134_AddSparteLastSynchronisation.Designer.cs
+++ Assfinet.Shared/Migrations/20240807111134_AddSparteLastSynchronisation.Designer.cs
@@ -4,6 +4,7 @@ using Assfinet.Shared;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Assfinet.Shared.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240807111134_AddSparteLastSynchronisation")]
+    partial class AddSparteLastSynchronisation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

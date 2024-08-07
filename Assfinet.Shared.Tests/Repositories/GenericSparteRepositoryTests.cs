5c38410e48e9da92d511807b9195c143f7025542
t-kuentzler <tobias.kuentzler@outlook.de>
2024-08-07 09:01:30 +0200

    Initial

--- Assfinet.Shared.Tests/Repositories/GenericSparteRepositoryTests.cs
+++ Assfinet.Shared.Tests/Repositories/GenericSparteRepositoryTests.cs
@@ -7,101 +7,101 @@ using Moq;
 
 namespace Assfinet.Shared.Tests.Repositories
 {
-    public class GenericSparteRepositoryTests
-    {
-        private readonly ApplicationDbContext _dbContext;
-        private readonly GenericSparteRepository<KrvSparte> _repository;
-
-        public GenericSparteRepositoryTests()
-        {
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(databaseName: "TestDatabase")
-                .Options;
-
-            _dbContext = new ApplicationDbContext(options);
-            _repository = new GenericSparteRepository<KrvSparte>(_dbContext);
-        }
-
-        [Fact]
-        public async Task AddAsync_ValidEntity_AddsEntityToDatabase()
-        {
-            // Arrange
-            var sparte = new KrvSparte { AmsId = Guid.NewGuid(), Key = "testKey", Amsidnr = "123", Typ = "KRV" };
-
-            // Act
-            await _repository.AddAsync(sparte);
-
-            // Assert
-            var entity = await _dbContext.Set<KrvSparte>().FirstOrDefaultAsync(s => s.AmsId == sparte.AmsId);
-            Assert.NotNull(entity);
-            Assert.Equal(sparte.AmsId, entity.AmsId);
-        }
-
-        [Fact]
-        public async Task AddAsync_ExceptionThrown_ThrowsRepositoryException()
-        {
-            // Arrange
-            var mockDbSet = new Mock<DbSet<KrvSparte>>();
-            mockDbSet.Setup(m => m.AddAsync(It.IsAny<KrvSparte>(), default)).Throws(new Exception("Test exception"));
-
-            var mockContext = new Mock<ApplicationDbContext>(new DbContextOptions<ApplicationDbContext>());
-            mockContext.Setup(m => m.Set<KrvSparte>()).Returns(mockDbSet.Object);
-
-            var repository = new GenericSparteRepository<KrvSparte>(mockContext.Object);
-
-            var sparte = new KrvSparte { AmsId = Guid.NewGuid(), Key = "testKey" };
-
-            // Act & Assert
-            var exception = await Assert.ThrowsAsync<RepositoryException>(() => repository.AddAsync(sparte));
-            Assert.Contains("Ein unerwarteter Fehler ist beim Hinzufügen von 'KrvSparte' aufgetreten.",
-                exception.Message);
-        }
-
-        [Fact]
-        public async Task GetSparteByAmsIdAsync_ValidAmsId_ReturnsEntity()
-        {
-            // Arrange
-            var sparte = new KrvSparte { AmsId = Guid.NewGuid(), Key = "testKey", Amsidnr = "123", Typ = "KRV" };
-            _dbContext.Set<KrvSparte>().Add(sparte);
-            await _dbContext.SaveChangesAsync();
-
-            // Act
-            var result = await _repository.GetSparteByAmsIdAsync(sparte.AmsId);
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.Equal(sparte.AmsId, result.AmsId);
-        }
-
-        [Fact]
-        public async Task GetSparteByAmsIdAsync_InvalidAmsId_ReturnsNull()
-        {
-            // Arrange
-            var invalidAmsId = Guid.NewGuid();
-
-            // Act
-            var result = await _repository.GetSparteByAmsIdAsync(invalidAmsId);
-
-            // Assert
-            Assert.Null(result);
-        }
-
-        [Fact]
-        public async Task GetSparteByAmsIdAsync_ExceptionThrown_ThrowsRepositoryException()
-        {
-            // Arrange
-            var mockDbSet = new Mock<DbSet<KrvSparte>>();
-            mockDbSet.Setup(m => m.Add(It.IsAny<KrvSparte>())).Throws(new Exception("Test exception"));
-
-            var mockContext = new Mock<ApplicationDbContext>(new DbContextOptions<ApplicationDbContext>());
-            mockContext.Setup(m => m.Set<KrvSparte>()).Returns(mockDbSet.Object);
-
-            var repository = new GenericSparteRepository<KrvSparte>(mockContext.Object);
-
-            // Act & Assert
-            var exception =
-                await Assert.ThrowsAsync<RepositoryException>(() => repository.GetSparteByAmsIdAsync(Guid.NewGuid()));
-            Assert.Contains("Ein unerwarteter Fehler ist beim Abrufen von 'KrvSparte' mit AmsId", exception.Message);
-        }
-    }
+    // public class GenericSparteRepositoryTests
+    // {
+    //     private readonly ApplicationDbContext _dbContext;
+    //     private readonly GenericSparteRepository<KrvSparte> _repository;
+    //
+    //     public GenericSparteRepositoryTests()
+    //     {
+    //         var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+    //             .UseInMemoryDatabase(databaseName: "TestDatabase")
+    //             .Options;
+    //
+    //         _dbContext = new ApplicationDbContext(options);
+    //         _repository = new GenericSparteRepository<KrvSparte>(_dbContext);
+    //     }
+    //
+    //     [Fact]
+    //     public async Task AddAsync_ValidEntity_AddsEntityToDatabase()
+    //     {
+    //         // Arrange
+    //         var sparte = new KrvSparte { AmsId = Guid.NewGuid(), Key = "testKey", Amsidnr = "123", Typ = "KRV" };
+    //
+    //         // Act
+    //         await _repository.AddAsync(sparte);
+    //
+    //         // Assert
+    //         var entity = await _dbContext.Set<KrvSparte>().FirstOrDefaultAsync(s => s.AmsId == sparte.AmsId);
+    //         Assert.NotNull(entity);
+    //         Assert.Equal(sparte.AmsId, entity.AmsId);
+    //     }
+    //
+    //     [Fact]
+    //     public async Task AddAsync_ExceptionThrown_ThrowsRepositoryException()
+    //     {
+    //         // Arrange
+    //         var mockDbSet = new Mock<DbSet<KrvSparte>>();
+    //         mockDbSet.Setup(m => m.AddAsync(It.IsAny<KrvSparte>(), default)).Throws(new Exception("Test exception"));
+    //
+    //         var mockContext = new Mock<ApplicationDbContext>(new DbContextOptions<ApplicationDbContext>());
+    //         mockContext.Setup(m => m.Set<KrvSparte>()).Returns(mockDbSet.Object);
+    //
+    //         var repository = new GenericSparteRepository<KrvSparte>(mockContext.Object);
+    //
+    //         var sparte = new KrvSparte { AmsId = Guid.NewGuid(), Key = "testKey" };
+    //
+    //         // Act & Assert
+    //         var exception = await Assert.ThrowsAsync<RepositoryException>(() => repository.AddAsync(sparte));
+    //         Assert.Contains("Ein unerwarteter Fehler ist beim Hinzufügen von 'KrvSparte' aufgetreten.",
+    //             exception.Message);
+    //     }
+    //
+    //     [Fact]
+    //     public async Task GetSparteByAmsIdAsync_ValidAmsId_ReturnsEntity()
+    //     {
+    //         // Arrange
+    //         var sparte = new KrvSparte { AmsId = Guid.NewGuid(), Key = "testKey", Amsidnr = "123", Typ = "KRV" };
+    //         _dbContext.Set<KrvSparte>().Add(sparte);
+    //         await _dbContext.SaveChangesAsync();
+    //
+    //         // Act
+    //         var result = await _repository.GetSparteByAmsIdAsync(sparte.AmsId);
+    //
+    //         // Assert
+    //         Assert.NotNull(result);
+    //         Assert.Equal(sparte.AmsId, result.AmsId);
+    //     }
+    //
+    //     [Fact]
+    //     public async Task GetSparteByAmsIdAsync_InvalidAmsId_ReturnsNull()
+    //     {
+    //         // Arrange
+    //         var invalidAmsId = Guid.NewGuid();
+    //
+    //         // Act
+    //         var result = await _repository.GetSparteByAmsIdAsync(invalidAmsId);
+    //
+    //         // Assert
+    //         Assert.Null(result);
+    //     }
+    //
+    //     [Fact]
+    //     public async Task GetSparteByAmsIdAsync_ExceptionThrown_ThrowsRepositoryException()
+    //     {
+    //         // Arrange
+    //         var mockDbSet = new Mock<DbSet<KrvSparte>>();
+    //         mockDbSet.Setup(m => m.Add(It.IsAny<KrvSparte>())).Throws(new Exception("Test exception"));
+    //
+    //         var mockContext = new Mock<ApplicationDbContext>(new DbContextOptions<ApplicationDbContext>());
+    //         mockContext.Setup(m => m.Set<KrvSparte>()).Returns(mockDbSet.Object);
+    //
+    //         var repository = new GenericSparteRepository<KrvSparte>(mockContext.Object);
+    //
+    //         // Act & Assert
+    //         var exception =
+    //             await Assert.ThrowsAsync<RepositoryException>(() => repository.GetSparteByAmsIdAsync(Guid.NewGuid()));
+    //         Assert.Contains("Ein unerwarteter Fehler ist beim Abrufen von 'KrvSparte' mit AmsId", exception.Message);
+    //     }
+    // }
 }
--- Assfinet.Shared.Tests/Services/SparteProcessingServiceTests.cs
+++ Assfinet.Shared.Tests/Services/SparteProcessingServiceTests.cs
@@ -8,133 +8,133 @@ using Assfinet.Shared.Entities;
 
 namespace Assfinet.Shared.Tests.Services
 {
-    public class SparteProcessingServiceTests
-    {
-        private readonly Mock<IVertragRepository> _vertragRepositoryMock;
-        private readonly Mock<IAppLogger> _loggerMock;
-        private readonly Mock<ISparteRepository> _sparteRepositoryMock;
-        
-        //Für die dynamische erstellung der validatoren
-        private readonly Mock<IServiceProvider> _serviceProviderMock;
-        private readonly SparteProcessingService _sparteProcessingService;
-
-        public SparteProcessingServiceTests()
-        {
-            _vertragRepositoryMock = new Mock<IVertragRepository>();
-            _loggerMock = new Mock<IAppLogger>();
-            _sparteRepositoryMock = new Mock<ISparteRepository>();
-            _serviceProviderMock = new Mock<IServiceProvider>();
-
-            _sparteProcessingService = new SparteProcessingService(
-                _vertragRepositoryMock.Object,
-                _loggerMock.Object,
-                _sparteRepositoryMock.Object,
-                _serviceProviderMock.Object
-            );
-        }
-
-        [Fact]
-        public async Task ValidateSparteAsync_NoValidatorFound_ThrowsInvalidOperationException()
-        {
-            // Arrange
-            var sparte = new SparteTestModel { Key = "testKey" };
-            _serviceProviderMock.Setup(sp => sp.GetService(It.IsAny<Type>())).Returns(null);
-
-            // Act & Assert
-            var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _sparteProcessingService.ValidateSparteAsync(sparte));
-            Assert.Contains("Kein Validator für den Typ 'SparteTestModel' gefunden.", exception.Message);
-        }
-
-        [Fact]
-        public async Task ValidateSparteAsync_ValidationFails_ThrowsValidationException()
-        {
-            // Arrange
-            var sparte = new KrvSparte { Key = "testKey", AmsId = Guid.NewGuid() };
-            var validatorMock = new Mock<IValidator<KrvSparte>>();
-            var validationResult = new ValidationResult(new List<ValidationFailure> { new ValidationFailure("Key", "Validation failed") });
-
-            _serviceProviderMock.Setup(sp => sp.GetService(typeof(IValidator<KrvSparte>)))
-                .Returns(validatorMock.Object);
-
-            validatorMock.Setup(v => v.ValidateAsync(It.IsAny<IValidationContext>(), default))
-                .ReturnsAsync(validationResult);
-
-            // Act & Assert
-            var exception = await Assert.ThrowsAsync<ValidationException>(() => _sparteProcessingService.ValidateSparteAsync(sparte));
-            Assert.Contains("Validation failed", exception.Message);
-        }
-
-
-        [Fact]
-        public async Task ProcessImportSparteAsync_NoKeyProperty_ThrowsInvalidOperationException()
-        {
-            // Arrange
-            var sparte = new { AmsId = Guid.NewGuid() };
-
-            // Act & Assert
-            var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _sparteProcessingService.ProcessImportSparteAsync(sparte));
-            Assert.Contains("Die Eigenschaften 'Key' und 'AmsId' müssen im Typ '<>f__AnonymousType0`1' vorhanden sein.", exception.Message);
-        }
-
-        [Fact]
-        public async Task ProcessImportSparteAsync_NoAmsIdProperty_ThrowsInvalidOperationException()
-        {
-            // Arrange
-            var sparte = new { Key = "testKey" };
-
-            // Act & Assert
-            var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _sparteProcessingService.ProcessImportSparteAsync(sparte));
-            Assert.Contains("Die Eigenschaften 'Key' und 'AmsId' müssen", exception.Message);
-        }
-
-        [Fact]
-        public async Task ProcessImportSparteAsync_NoVertragFound_LogsError()
-        {
-            // Arrange
-            var sparte = new SparteTestEntity { Key = "testKey", AmsId = Guid.NewGuid() };
-
-            _vertragRepositoryMock.Setup(v => v.GetVertragByAmsidnrAsync(It.IsAny<string>())).ReturnsAsync((Vertrag?)null);
-
-            // Act
-            await _sparteProcessingService.ProcessImportSparteAsync(sparte);
-
-            // Assert
-            _loggerMock.Verify(l => l.LogError(It.Is<string>(s => s.Contains("Die Spartendaten mit dem Key 'testKey' im Typ 'SparteTestEntity' konnten nicht in der Datenbank erstellt werden, da kein Vertrag mit der entsprechenden Amsidnr gefunden wurde."))), Times.Once);
-        }
-
-        [Fact]
-        public async Task ProcessImportSparteAsync_SparteAlreadyExists_LogsError()
-        {
-            // Arrange
-            var sparte = new SparteTestEntity { Key = "testKey", AmsId = Guid.NewGuid() };
-            var vertrag = new Vertrag();
-
-            _vertragRepositoryMock.Setup(v => v.GetVertragByAmsidnrAsync(It.IsAny<string>())).ReturnsAsync(vertrag);
-            _sparteRepositoryMock.Setup(s => s.GetSparteByAmsIdAsync(It.IsAny<Guid>(), It.IsAny<Type>())).ReturnsAsync(new object());
-
-            // Act
-            await _sparteProcessingService.ProcessImportSparteAsync(sparte);
-
-            // Assert
-            _loggerMock.Verify(l => l.LogError(It.Is<string>(s => s.Contains("Die Spartendaten mit der AmsId"))), Times.Once);
-        }
-
-        [Fact]
-        public async Task ProcessImportSparteAsync_SuccessfullyCreatesSparte_LogsInformation()
-        {
-            // Arrange
-            var sparte = new SparteTestEntity { Key = "testKey", AmsId = Guid.NewGuid() };
-            var vertrag = new Vertrag();
-
-            _vertragRepositoryMock.Setup(v => v.GetVertragByAmsidnrAsync(It.IsAny<string>())).ReturnsAsync(vertrag);
-            _sparteRepositoryMock.Setup(s => s.GetSparteByAmsIdAsync(It.IsAny<Guid>(), It.IsAny<Type>())).ReturnsAsync((object?)null);
-            _sparteRepositoryMock.Setup(s => s.AddAsync(It.IsAny<object>())).Returns(Task.CompletedTask);
-
-            // Act
-            await _sparteProcessingService.ProcessImportSparteAsync(sparte);
-
-            // Assert
-            _loggerMock.Verify(l => l.LogInformation(It.Is<string>(s => s.Contains("Die Spartendaten mit der AmsId"))), Times.Once);
-        }
-    }
+    // public class SparteProcessingServiceTests
+    // {
+    //     private readonly Mock<IVertragRepository> _vertragRepositoryMock;
+    //     private readonly Mock<IAppLogger> _loggerMock;
+    //     private readonly Mock<ISparteRepository> _sparteRepositoryMock;
+    //     
+    //     //Für die dynamische erstellung der validatoren
+    //     private readonly Mock<IServiceProvider> _serviceProviderMock;
+    //     private readonly SparteProcessingService _sparteProcessingService;
+    //
+    //     public SparteProcessingServiceTests()
+    //     {
+    //         _vertragRepositoryMock = new Mock<IVertragRepository>();
+    //         _loggerMock = new Mock<IAppLogger>();
+    //         _sparteRepositoryMock = new Mock<ISparteRepository>();
+    //         _serviceProviderMock = new Mock<IServiceProvider>();
+    //
+    //         _sparteProcessingService = new SparteProcessingService(
+    //             _vertragRepositoryMock.Object,
+    //             _loggerMock.Object,
+    //             _sparteRepositoryMock.Object,
+    //             _serviceProviderMock.Object
+    //         );
+    //     }
+    //
+    //     [Fact]
+    //     public async Task ValidateSparteAsync_NoValidatorFound_ThrowsInvalidOperationException()
+    //     {
+    //         // Arrange
+    //         var sparte = new SparteTestModel { Key = "testKey" };
+    //         _serviceProviderMock.Setup(sp => sp.GetService(It.IsAny<Type>())).Returns(null);
+    //
+    //         // Act & Assert
+    //         var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _sparteProcessingService.ValidateSparteAsync(sparte));
+    //         Assert.Contains("Kein Validator für den Typ 'SparteTestModel' gefunden.", exception.Message);
+    //     }
+    //
+    //     [Fact]
+    //     public async Task ValidateSparteAsync_ValidationFails_ThrowsValidationException()
+    //     {
+    //         // Arrange
+    //         var sparte = new KrvSparte { Key = "testKey", AmsId = Guid.NewGuid() };
+    //         var validatorMock = new Mock<IValidator<KrvSparte>>();
+    //         var validationResult = new ValidationResult(new List<ValidationFailure> { new ValidationFailure("Key", "Validation failed") });
+    //
+    //         _serviceProviderMock.Setup(sp => sp.GetService(typeof(IValidator<KrvSparte>)))
+    //             .Returns(validatorMock.Object);
+    //
+    //         validatorMock.Setup(v => v.ValidateAsync(It.IsAny<IValidationContext>(), default))
+    //             .ReturnsAsync(validationResult);
+    //
+    //         // Act & Assert
+    //         var exception = await Assert.ThrowsAsync<ValidationException>(() => _sparteProcessingService.ValidateSparteAsync(sparte));
+    //         Assert.Contains("Validation failed", exception.Message);
+    //     }
+    //
+    //
+    //     [Fact]
+    //     public async Task ProcessImportSparteAsync_NoKeyProperty_ThrowsInvalidOperationException()
+    //     {
+    //         // Arrange
+    //         var sparte = new { AmsId = Guid.NewGuid() };
+    //
+    //         // Act & Assert
+    //         var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _sparteProcessingService.ProcessImportSparteAsync(sparte));
+    //         Assert.Contains("Die Eigenschaften 'Key' und 'AmsId' müssen im Typ '<>f__AnonymousType0`1' vorhanden sein.", exception.Message);
+    //     }
+    //
+    //     [Fact]
+    //     public async Task ProcessImportSparteAsync_NoAmsIdProperty_ThrowsInvalidOperationException()
+    //     {
+    //         // Arrange
+    //         var sparte = new { Key = "testKey" };
+    //
+    //         // Act & Assert
+    //         var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => _sparteProcessingService.ProcessImportSparteAsync(sparte));
+    //         Assert.Contains("Die Eigenschaften 'Key' und 'AmsId' müssen", exception.Message);
+    //     }
+    //
+    //     [Fact]
+    //     public async Task ProcessImportSparteAsync_NoVertragFound_LogsError()
+    //     {
+    //         // Arrange
+    //         var sparte = new SparteTestEntity { Key = "testKey", AmsId = Guid.NewGuid() };
+    //
+    //         _vertragRepositoryMock.Setup(v => v.GetVertragByAmsidnrAsync(It.IsAny<string>())).ReturnsAsync((Vertrag?)null);
+    //
+    //         // Act
+    //         await _sparteProcessingService.ProcessImportSparteAsync(sparte);
+    //
+    //         // Assert
+    //         _loggerMock.Verify(l => l.LogError(It.Is<string>(s => s.Contains("Die Spartendaten mit dem Key 'testKey' im Typ 'SparteTestEntity' konnten nicht in der Datenbank erstellt werden, da kein Vertrag mit der entsprechenden Amsidnr gefunden wurde."))), Times.Once);
+    //     }
+    //
+    //     [Fact]
+    //     public async Task ProcessImportSparteAsync_SparteAlreadyExists_LogsError()
+    //     {
+    //         // Arrange
+    //         var sparte = new SparteTestEntity { Key = "testKey", AmsId = Guid.NewGuid() };
+    //         var vertrag = new Vertrag();
+    //
+    //         _vertragRepositoryMock.Setup(v => v.GetVertragByAmsidnrAsync(It.IsAny<string>())).ReturnsAsync(vertrag);
+    //         _sparteRepositoryMock.Setup(s => s.GetSparteByAmsIdAsync(It.IsAny<Guid>(), It.IsAny<Type>())).ReturnsAsync(new object());
+    //
+    //         // Act
+    //         await _sparteProcessingService.ProcessImportSparteAsync(sparte);
+    //
+    //         // Assert
+    //         _loggerMock.Verify(l => l.LogError(It.Is<string>(s => s.Contains("Die Spartendaten mit der AmsId"))), Times.Once);
+    //     }
+    //
+    //     [Fact]
+    //     public async Task ProcessImportSparteAsync_SuccessfullyCreatesSparte_LogsInformation()
+    //     {
+    //         // Arrange
+    //         var sparte = new SparteTestEntity { Key = "testKey", AmsId = Guid.NewGuid() };
+    //         var vertrag = new Vertrag();
+    //
+    //         _vertragRepositoryMock.Setup(v => v.GetVertragByAmsidnrAsync(It.IsAny<string>())).ReturnsAsync(vertrag);
+    //         _sparteRepositoryMock.Setup(s => s.GetSparteByAmsIdAsync(It.IsAny<Guid>(), It.IsAny<Type>())).ReturnsAsync((object?)null);
+    //         _sparteRepositoryMock.Setup(s => s.AddAsync(It.IsAny<object>())).Returns(Task.CompletedTask);
+    //
+    //         // Act
+    //         await _sparteProcessingService.ProcessImportSparteAsync(sparte);
+    //
+    //         // Assert
+    //         _loggerMock.Verify(l => l.LogInformation(It.Is<string>(s => s.Contains("Die Spartendaten mit der AmsId"))), Times.Once);
+    //     }
+    // }
 }

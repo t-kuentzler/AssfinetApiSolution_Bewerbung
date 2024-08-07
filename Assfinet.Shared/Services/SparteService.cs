fb829f20a863df5f798a44dbeceaf28589fe46ca
t-kuentzler <tobias.kuentzler@outlook.de>
2024-08-07 11:35:55 +0200

    AutoMapper entfernt

--- Assfinet.Shared/Services/SparteService.cs
+++ Assfinet.Shared/Services/SparteService.cs
@@ -1,7 +1,6 @@
 using Assfinet.Shared.Contracts;
 using Assfinet.Shared.Entities;
 using Assfinet.Shared.Exceptions;
-using AutoMapper;
 using FluentValidation;
 
 namespace Assfinet.Shared.Services
@@ -11,18 +10,15 @@ namespace Assfinet.Shared.Services
         private readonly ISparteParserService _sparteParserService;
         private readonly IAppLogger _logger;
         private readonly ISparteProcessingService _sparteProcessingService;
-        private readonly IMapper _mapper;
 
         public SparteService(
             ISparteParserService sparteParserService,
             IAppLogger logger,
-            ISparteProcessingService sparteProcessingService,
-            IMapper mapper)
+            ISparteProcessingService sparteProcessingService)
         {
             _sparteParserService = sparteParserService;
             _logger = logger;
             _sparteProcessingService = sparteProcessingService;
-            _mapper = mapper;
         }
 
         public async Task ImportSpartenDatenAsync(List<object> spartenModels)
@@ -46,32 +42,44 @@ namespace Assfinet.Shared.Services
                 }
                 catch (ArgumentNullException ex)
                 {
-                    _logger.LogError($"ArgumentNullException beim Importieren von den Spartendaten mit dem Key '{(sparteModel as dynamic).Key}': {ex.Message}", ex);
+                    _logger.LogError(
+                        $"ArgumentNullException beim Importieren von den Spartendaten mit dem Key '{(sparteModel as dynamic).Key}': {ex.Message}",
+                        ex);
                     throw;
                 }
                 catch (InvalidOperationException ex)
                 {
-                    _logger.LogError($"InvalidOperationException beim Importieren von den Spartendaten mit dem Key '{(sparteModel as dynamic).Key}': {ex.Message}", ex);
+                    _logger.LogError(
+                        $"InvalidOperationException beim Importieren von den Spartendaten mit dem Key '{(sparteModel as dynamic).Key}': {ex.Message}",
+                        ex);
                     throw;
                 }
                 catch (ValidationException ex)
                 {
-                    _logger.LogError($"Validierungsfehler bei Spartendaten mit dem Key '{(sparteModel as dynamic).Key}': {ex.Message}", ex);
+                    _logger.LogError(
+                        $"Validierungsfehler bei Spartendaten mit dem Key '{(sparteModel as dynamic).Key}': {ex.Message}",
+                        ex);
                     throw;
                 }
                 catch (UnknownSparteException ex)
                 {
-                    _logger.LogError($"UnknownSparteException beim Importieren von den Spartendaten mit dem Key '{(sparteModel as dynamic).Key}': {ex.Message}", ex);
+                    _logger.LogError(
+                        $"UnknownSparteException beim Importieren von den Spartendaten mit dem Key '{(sparteModel as dynamic).Key}': {ex.Message}",
+                        ex);
                     throw;
                 }
                 catch (RepositoryException ex)
                 {
-                    _logger.LogError($"Repository-Fehler beim Import von Spartendaten mit dem Key '{(sparteModel as dynamic).Key}'.", ex);
+                    _logger.LogError(
+                        $"Repository-Fehler beim Import von Spartendaten mit dem Key '{(sparteModel as dynamic).Key}'.",
+                        ex);
                     throw;
                 }
                 catch (Exception ex)
                 {
-                    _logger.LogError($"Unerwarteter Fehler beim Import von Spartendaten mit dem Key '{(sparteModel as dynamic).Key}'.", ex);
+                    _logger.LogError(
+                        $"Unerwarteter Fehler beim Import von Spartendaten mit dem Key '{(sparteModel as dynamic).Key}'.",
+                        ex);
                     throw new SparteServiceException();
                 }
             }
